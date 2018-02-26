477371d4fca0ae9f5a77dbf6a247b0c7a90b1e02
Nicholas Lima <nicholaslima.rw@gmail.com>
2018-02-26 14:24:05 -0300

    Blocking nvidia on default, and blocking nouveau on nvidia

--- BlacklistNvidia.sh
+++ BlacklistNvidia.sh
@@ -20,6 +20,9 @@ printf "Configuring...\n"
 printf "Original boot options with Nvidia modules disabled!\n"
 configFile=`ls /boot/loader/entries/ | grep $systemRelease`
 echo $configFile
+if [[ `sudo cat /boot/loader/entries/$configFile | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
+    sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
+fi
 sudo cp -f /boot/loader/entries/$configFile /boot/loader/entries/nvidia.conf
 # # Enables nouveau by default
 # sudo sed -i 's/\<rd.driver.blacklist=nouveau\> //g' /boot/loader/entries/$configFile
@@ -34,6 +37,7 @@ sudo cp -f /boot/loader/entries/$configFile /boot/loader/entries/nvidia.conf
 #
 sudo cat /boot/loader/entries/nvidia.conf
 sudo sed -i '/title/s/$/ Nvidia/' /boot/loader/entries/nvidia.conf
+#sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/nvidia.conf
 if [[ `sudo cat /boot/loader/entries/nvidia.conf | grep rd.driver.blacklist=nouveau` == '' ]]; then
     sudo sed -i '/options/s/$/ rd.driver.blacklist=nouveau/' /boot/loader/entries/nvidia.conf
 fi
@@ -43,5 +47,8 @@ fi
 if [[ `sudo cat /boot/loader/entries/nvidia.conf | grep nvidia-drm.modeset=1` == '' ]]; then
     sudo sed -i '/options/s/$/ nvidia-drm.modeset=1/' /boot/loader/entries/nvidia.conf
 fi
+#if ! [ $(sudo cat /boot/loader/entries/nvidia.conf | grep "modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm") == "" ]; then
+sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/nvidia.conf
+#fi
 
 sudo cat /boot/loader/entries/nvidia.conf
