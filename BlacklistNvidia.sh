f2ee99c54b3e57176584441740f56880e858b678
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-02-26 17:57:29 -0300

    Cleaning the code

--- BlacklistNvidia.sh
+++ BlacklistNvidia.sh
@@ -18,7 +18,7 @@ printf "\n----------------------\n\n"
 printf "Configuring...\n"
 printf "Original boot options with Nvidia modules disabled!\n"
 configFile=`ls /boot/loader/entries/ | grep $systemRelease`
-echo $configFile
+
 if [[ `sudo cat /boot/loader/entries/$configFile | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
     sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
 fi
@@ -28,15 +28,8 @@ sudo cp -f /boot/loader/entries/$configFile /boot/loader/entries/nvidia.conf
 # sudo sed -i 's/\<modprobe.blacklist=nouveau\> //g' /boot/loader/entries/$configFile
 # sudo sed -i 's/\<nvidia-drm.modeset=1\> //g' /boot/loader/entries/$configFile
 #
-# sudo cat /boot/loader/entries/$configFile
-#
-# printf "\nNew boot menu entry with Nvidia modules enabled:\n"
-#
-# modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm
-#
-sudo cat /boot/loader/entries/nvidia.conf
 sudo sed -i '/title/s/$/ Nvidia/' /boot/loader/entries/nvidia.conf
-#sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/nvidia.conf
+
 if [[ `sudo cat /boot/loader/entries/nvidia.conf | grep rd.driver.blacklist=nouveau` == '' ]]; then
     sudo sed -i '/options/s/$/ rd.driver.blacklist=nouveau/' /boot/loader/entries/nvidia.conf
 fi
@@ -46,8 +39,6 @@ fi
 if [[ `sudo cat /boot/loader/entries/nvidia.conf | grep nvidia-drm.modeset=1` == '' ]]; then
     sudo sed -i '/options/s/$/ nvidia-drm.modeset=1/' /boot/loader/entries/nvidia.conf
 fi
-#if ! [ $(sudo cat /boot/loader/entries/nvidia.conf | grep "modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm") == "" ]; then
-sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/nvidia.conf
-#fi
 
-sudo cat /boot/loader/entries/nvidia.conf
+sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/nvidia.conf
+printf "\nNew boot menu entry with Nvidia modules enabled\n"
