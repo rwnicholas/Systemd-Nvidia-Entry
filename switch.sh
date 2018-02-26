7f6b6568ef2c0b1380764dd8e4f2e007fb3f35ba
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-02-26 17:41:13 -0300

    echos

--- switch.sh
+++ switch.sh
@@ -8,8 +8,10 @@ if [[ `sudo cat /boot/loader/loader.conf | grep $systemRelease` == "default $sys
     sudo sed -i "s/$systemRelease/nvidia/g" /boot/loader/loader.conf
 	sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
 	sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+	echo "Nvidia"
 else
 	sudo sed -i "s/nvidia/$systemRelease/g" /boot/loader/loader.conf
 	sudo mv -f /etc/X11/xorg.conf.d/00-ldm.conf ~/.cache/MarechalLima/00-ldm.conf
 	sudo sed -i "s/blacklist/#blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+	echo "Nouveau"
 fi
