3605f5b399f5ed2d399e36ba90e43d2f4d2c9242
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-02-26 17:40:05 -0300

    switching between nvidia and nouveau

--- switch.sh
+++ switch.sh
@@ -1,5 +1,15 @@
-configFile=`ls /boot/loader/entries/ | grep $systemRelease`
-echo $configFile
-# if [[ `sudo cat /boot/loader/loader.conf | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
-#     sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
-# fi
+systemRelease="Solus"
+systemRelease=`ls /boot/loader/entries/ | grep $systemRelease`
+systemRelease=${systemRelease/.conf/}
+if ! [ -d ~/.cache/MarechalLima/ ]; then
+	mkdir ~/.cache/MarechalLima/
+fi
+if [[ `sudo cat /boot/loader/loader.conf | grep $systemRelease` == "default $systemRelease" ]]; then
+    sudo sed -i "s/$systemRelease/nvidia/g" /boot/loader/loader.conf
+	sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
+	sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+else
+	sudo sed -i "s/nvidia/$systemRelease/g" /boot/loader/loader.conf
+	sudo mv -f /etc/X11/xorg.conf.d/00-ldm.conf ~/.cache/MarechalLima/00-ldm.conf
+	sudo sed -i "s/blacklist/#blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+fi
