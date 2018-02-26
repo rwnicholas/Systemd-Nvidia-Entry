5c04b80e83b155bf2a58ff44d9799284d3366a83
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-02-26 19:14:23 -0300

    Sorry, at this point It is a solution for Solus OS

--- switchgpu.sh
+++ switchgpu.sh
@@ -1,17 +1,17 @@
-systemRelease="Solus"
-systemRelease=`ls /boot/loader/entries/ | grep $systemRelease`
-systemRelease=${systemRelease/.conf/}
+configFile="Solus"
+configFile=`ls /boot/loader/entries/ | grep $configFile`
+#systemRelease=${systemRelease/.conf/}
 if ! [ -d ~/.cache/MarechalLima/ ]; then
 	mkdir ~/.cache/MarechalLima/
 fi
-if [[ `sudo cat /boot/loader/loader.conf | grep $systemRelease` == "default $systemRelease" ]]; then
-    sudo sed -i "s/$systemRelease/nvidia/g" /boot/loader/loader.conf
-	sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
-	sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
-	echo "Nvidia"
-else
-	sudo sed -i "s/nvidia/$systemRelease/g" /boot/loader/loader.conf
+if [[ `lsmod | grep nouveau` == '' ]]; then
+	sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
 	sudo mv -f /etc/X11/xorg.conf.d/00-ldm.conf ~/.cache/MarechalLima/00-ldm.conf
 	sudo sed -i "s/blacklist/#blacklist/g" /usr/lib/modprobe.d/nvidia.conf
 	echo "Nouveau"
+else
+	sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/$configFile
+	sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
+	sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+	echo "Nvidia"
 fi
