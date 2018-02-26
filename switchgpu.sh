a590d5936bd89e374526c176782e9dfee24ac615
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-02-26 20:15:24 -0300

    Verifying if the script can run

--- switchgpu.sh
+++ switchgpu.sh
@@ -5,13 +5,21 @@ if ! [ -d ~/.cache/MarechalLima/ ]; then
 	mkdir ~/.cache/MarechalLima/
 fi
 if [[ `lsmod | grep nouveau` == '' ]]; then
-	sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
-	sudo mv -f /etc/X11/xorg.conf.d/00-ldm.conf ~/.cache/MarechalLima/00-ldm.conf
-	sudo sed -i "s/blacklist/#blacklist/g" /usr/lib/modprobe.d/nvidia.conf
-	echo "Nouveau"
+	if [[ `sudo cat /boot/loader/entries/$configFile | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
+		sudo sed -i '/options/s/$/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm/' /boot/loader/entries/$configFile
+		sudo mv -f /etc/X11/xorg.conf.d/00-ldm.conf ~/.cache/MarechalLima/00-ldm.conf
+		sudo sed -i "s/blacklist/#blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+		echo "Nouveau"
+	else
+		echo "Already on Nouveau! Please Reboot"
+	fi
 else
-	sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /boot/loader/entries/$configFile
-	sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
-	sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
-	echo "Nvidia"
+	if ! [[ `sudo cat /boot/loader/entries/$configFile | grep modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm` == '' ]]; then
+		sudo mv -f ~/.cache/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf
+		sudo sed -i "s/#blacklist/blacklist/g" /usr/lib/modprobe.d/nvidia.conf
+		sudo sed -i 's/ modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm//g' /boot/loader/entries/$configFile
+		echo "Nvidia"
+	else
+		echo "Already on Nvidia! Please Reboot"
+	fi
 fi
