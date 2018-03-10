f5bdd14ab73ed2364b9b141b26d65062b94acc70
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-03-10 11:05:58 -0300

    os.getenv for get username

--- solus-prime-indicator/trayicon.py
+++ solus-prime-indicator/trayicon.py
@@ -38,23 +38,20 @@ class App:
 
 	def switch(self):
 		try:
-			#os.system('pkexec --user $(logname) /usr/bin/gpu-switch')
-			getUser = str(check_output("logname"))
-			getUser = getUser.replace("'", "")
-			getUser = getUser[:-2]
-			getUser = getUser[1:]
+			getUser = os.getenv('USER')
 			status = str(check_output(["pkexec", "/usr/bin/gpu-switch", getUser]))
 			status = status.replace("'", "")
 			status = status[:-2]
 			status = status[1:]
+			print(status)
 			if status == "nouveau":
-				check_call(['notify-send', '-i', 'dialog-information', '"Nouveau, please reboot!"'])
+				check_call(['notify-send', '-i', 'dialog-information', 'Nouveau, please reboot!'])
 			elif status == "nouveau!":
-				check_call(['notify-send', '-i', 'dialog-information', '"Already Nouveau, please reboot!"'])
+				check_call(['notify-send', '-i', 'dialog-information', 'Already Nouveau, please reboot!'])
 			elif status == "nvidia":
-				check_call(['notify-send', '-i', 'nvidia', '"Nvidia, please reboot!"'])
+				check_call(['notify-send', '-i', 'nvidia', 'Nvidia, please reboot!'])
 			elif status == "nvidia!":
-				check_call(['notify-send', '-i', 'nvidia', '"Already Nvidia, please reboot!"'])
+				check_call(['notify-send', '-i', 'nvidia', 'Already Nvidia, please reboot!'])
 		except Exception as e:
 			print(e)
 
