78c2b1d3e4282e689feef6d7bfb099f6d8306391
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-04-07 23:19:24 -0300

    removing all the changes

--- systemd-nvidia-entry/Setup.sh
+++ systemd-nvidia-entry/Setup.sh
@@ -14,6 +14,28 @@ install(){
 }
 
 uninstall(){
+	partitionEFI=$(lsblk -o NAME,FSTYPE -l | grep vfat)
+	partitionEFI=${partitionEFI::-5}
+	if ! [[ `cat /proc/mounts | grep /mnt` == "" ]]; then
+		printf "\n/mnt is busy! Please unmount /mnt!"
+		printf "\nExiting...\n"
+		exit    
+	fi
+
+	printf "\n----------------------\n\n"
+	printf "Mounting EFI Partition ($partitionEFI)\n"
+	sudo mount /dev/$partitionEFI /mnt
+
+	sudo rm -f /mnt/loader/entries/nvidia.conf
+	configFile=$(uname -r)
+	if [[ `echo $configFile | grep current` ]]; then
+		configFile=${configFile::-8}
+	else
+		configFile=${configFile::-4}
+	fi
+	configFile=$(ls /mnt/loader/entries/ | grep $configFile)
+	sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /mnt/loader/entries/$configFile
+
 	if ! [[ -e /etc/X11/xorg.conf.d/00-ldm.conf ]]; then
 		sudo mv /opt/MarechalLima/00-ldm.conf /etc/X11/xorg.conf.d/00-ldm.conf -f
 	fi
@@ -22,6 +44,7 @@ uninstall(){
 	sudo systemctl disable systemd-nvidia-entry.service
 	sudo rm /etc/systemd/system/systemd-nvidia-entry.service
 	sudo sed -i 's/\<modprobe.blacklist=nvidia,nvidia_drm,nvidia_modeset,nvidia_uvm\> //g' /etc/kernel/cmdline
+	sudo umount /mnt
 }
 
 if [[ $1 == "rm" ]]; then
