683ec46a3c2b43f130a5b1ee80238c6ee82ea2f6
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2018-04-28 11:35:32 -0300

    Removing switch: Fix #10
    Systemd-Nvidia-Entry already creates options on bootloader for Intel and Nvidia.

--- solus-prime-indicator/trayicon.py
+++ solus-prime-indicator/trayicon.py
@@ -12,16 +12,16 @@ class App:
 		self.app = QApplication(sys.argv)
 		icon = None
 		try:
-			if 'nouveau' in str(check_output("lsmod")):
-				icon = QIcon.fromTheme("prime-intel")
-			else:
+			if 'nvidia' in str(check_output("lsmod")):
 				icon = QIcon.fromTheme("prime-nvidia")
+			else:
+				icon = QIcon.fromTheme("prime-intel")
 		except Exception as e:
 			print(e)
 		menu = QMenu()
 		#Gpu-switch
-		switchAction = menu.addAction("Gpu-Switch")
-		switchAction.triggered.connect(self.switch)
+		#switchAction = menu.addAction("Gpu-Switch")
+		#switchAction.triggered.connect(self.switch)
 		#Exit
 		exitAction = menu.addAction("Exit")
 		exitAction.triggered.connect(sys.exit)
@@ -36,25 +36,25 @@ class App:
 		self.app.exec_()
 		sys.exit()
 
-	def switch(self):
-		try:
-			getUser = os.getenv('USER')
-			status = str(check_output(["pkexec", "/usr/bin/gpu-switch", getUser]))
-			status = status.replace("'", "")
-			status = status[:-2]
-			status = status[1:]
+	# def switch(self):
+	# 	try:
+	# 		getUser = os.getenv('USER')
+	# 		status = str(check_output(["pkexec", "/usr/bin/gpu-switch", getUser]))
+	# 		status = status.replace("'", "")
+	# 		status = status[:-2]
+	# 		status = status[1:]
 
-			if status == "nouveau":
-				check_call(['notify-send', '-i', 'dialog-information', 'Nouveau, please reboot!'])
-			elif status == "nouveau!":
-				check_call(['notify-send', '-i', 'dialog-information', 'Already Nouveau, please reboot!'])
-			elif status == "nvidia":
-				check_call(['notify-send', '-i', 'nvidia', 'Nvidia, please reboot!'])
-			elif status == "nvidia!":
-				check_call(['notify-send', '-i', 'nvidia', 'Already Nvidia, please reboot!'])
-			check_call(["gnome-session-quit", "--reboot"])
-		except Exception as e:
-			print(e)
+	# 		if status == "nouveau":
+	# 			check_call(['notify-send', '-i', 'dialog-information', 'Nouveau, please reboot!'])
+	# 		elif status == "nouveau!":
+	# 			check_call(['notify-send', '-i', 'dialog-information', 'Already Nouveau, please reboot!'])
+	# 		elif status == "nvidia":
+	# 			check_call(['notify-send', '-i', 'nvidia', 'Nvidia, please reboot!'])
+	# 		elif status == "nvidia!":
+	# 			check_call(['notify-send', '-i', 'nvidia', 'Already Nvidia, please reboot!'])
+	# 		check_call(["gnome-session-quit", "--reboot"])
+	# 	except Exception as e:
+	# 		print(e)
 
 if __name__ == "__main__":
 	app = App()
